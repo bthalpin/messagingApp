fdf24bb7c978af6246314fbdd2f8eb8f67fb55a7
bthalpin <brian.t.halpin@gmail.com>
2021-07-21 14:46:21 -0400

    update

--- src/component/Messages/Picture.js
+++ src/component/Messages/Picture.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './Picture.css';
 
 const Picture = (source) => {
     console.log(source)
     return (
-        <div>
-            <img alt = "picture" src = {source.source} width = "200em"></img>
+        <div >
+            <img className = "picture" alt = "picture" src = {source.source} width = "200rem"></img>
         </div>
     )
 }
