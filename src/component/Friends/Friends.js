9e24661391f8bc0eb64b48c250710bc0900b9792
Brian Halpin <brian.t.halpin@gmail.com>
2022-02-04 23:40:48 -0500

    fixing typo

--- src/component/Friends/Friends.js
+++ src/component/Friends/Friends.js
@@ -101,7 +101,7 @@ const Friends = ({user,addFriend,converse}) => {
 
                                 </div>                        
                               
-                                {requests.length?
+                                {requests?
                                     <div className = "scroll">  
                                         <div className="friendtitlebox"><p className="requesttitle">Request</p></div>
                                         <ul className = "scroll">
@@ -120,7 +120,7 @@ const Friends = ({user,addFriend,converse}) => {
                                     :<></>
                                 }
                                 
-                                {pendingrequests.length?
+                                {pendingrequests?
                                     <div className = "scroll">
                                         <div className="friendtitlebox"><p className="pendingtitle">Pending Request</p></div>
                                         <ul className = "scroll">
